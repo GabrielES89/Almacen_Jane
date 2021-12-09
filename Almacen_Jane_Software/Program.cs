b9ebc6c5f6898ad035e2fc8fcc0f4429b8d4204d
GabrielES89 <GabrielEBV89@gmail.com>
2021-12-09 15:36:25 -0700

    Formas fuera

--- Almacen_Jane_Software/Program.cs
+++ Almacen_Jane_Software/Program.cs
@@ -16,7 +16,7 @@ namespace Almacen_Jane_Software
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Formas.FrmMenu.cs());
+            Application.Run(new FrmMenu());
         }
     }
 }
