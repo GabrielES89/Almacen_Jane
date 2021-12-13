e13fcd1820c84c615e695fbbec43d0b2507c106e
NoanHC <noanhc77@gmail.com>
2021-12-13 08:09:53 -0700

    add

--- Almacen_Jane_Software/Clases/ClsConexion.cs
+++ Almacen_Jane_Software/Clases/ClsConexion.cs
@@ -10,7 +10,7 @@ namespace Almacen_Jane_Software
     class ClsConexion
     {
         //static string Conexion = "datasource = 127.0.0.1; port = 3306; username = Gabriel; password = 123; database = almacen";
-        public static string Conexion = "datasource=127.0.0.1;port=8080;username=root;password=;database=db_almacen_jane;";
+        public static string Conexion = "datasource=localhost;port=3306;username=root;password=;database=db_almacen_jane;";
         public MySqlConnection Databaseconexion = new MySqlConnection(Conexion);
     }
 }
